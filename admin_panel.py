d1f8a95c0eb9cd0e29830a5a4dae872330668b43
dostqqe <yaroslava4548@gmail.com>
2025-04-23 20:59:41 +0300

    Update and rename sqladmin.py to admin_panel.py

--- admin_panel.py
+++ admin_panel.py
@@ -6,6 +6,7 @@ from fastapi_app.database import engine
 class PatientAdmin(ModelView, model=Patient):
     column_list = [Patient.id, Patient.name, Patient.age, Patient.sex, Patient.created_at]
     form_columns = [Patient.name, Patient.age, Patient.sex]
+    column_searchable_list = [Patient.name, Patient.age] #filter example
 
 def setup_admin(app):
     admin = Admin(app, engine)
